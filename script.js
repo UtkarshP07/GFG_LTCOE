ab443d172bcaf5151041e1cb5a296a8a544a235e
Utkarsh Pandey <140243045+UtkarshP07@users.noreply.github.com>
2024-02-15 20:35:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ function toggleMenu(button) {
   var navbarLinks = document.querySelector(".navbar-links");
   navbarLinks.classList.toggle("active");
 }
-function scrollToCouncil() {
-  document.getElementById("council").scrollIntoView({ behavior: "smooth" });
-}
 
-function scrollToEvent() {
-  document.getElementById("events").scrollIntoView({ behavior: "smooth" });
+function scrollToSection(sectionId, event) {
+  event.preventDefault(); // Prevent the default behavior of the anchor element
+  var section = document.getElementById(sectionId);
+  if (section) {
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: "smooth",
+    });
+  }
 }
