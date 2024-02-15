7f1d8862337411e87e0708b52a9b262915d48dc6
Utkarsh Pandey <140243045+UtkarshP07@users.noreply.github.com>
2024-02-16 05:06:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleMenu(button) {
 }
 
 function scrollToSection(sectionId, event) {
-  event.preventDefault(); // Prevent the default behavior of the anchor element
+  event.preventDefault();
   var section = document.getElementById(sectionId);
   if (section) {
     window.scrollTo({
@@ -13,4 +13,30 @@ function scrollToSection(sectionId, event) {
       behavior: "smooth",
     });
   }
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" slide-active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " slide-active";
+  setTimeout(showSlides, 2000); // Change slide every 3 seconds
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
 }
